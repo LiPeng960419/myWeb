284922827a3c8307e0f284927fa8c69453284ae7
lipeng <lp960419@163.com>
2020-10-30 13:21:12 +0800

    commit

--- src/main/java/com/lipeng/domain/LogMessage.java
+++ src/main/java/com/lipeng/domain/LogMessage.java
@@ -2,8 +2,10 @@ package com.lipeng.domain;
 
 import java.util.Date;
 import lombok.Builder;
+import lombok.Data;
 
 @Builder
+@Data
 public class LogMessage {
 
     private Long id;
@@ -26,99 +28,4 @@ public class LogMessage {
 
     private Integer version;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getType() {
-        return type;
-    }
-
-    public void setType(Integer type) {
-        this.type = type;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content == null ? null : content.trim();
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source == null ? null : source.trim();
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public String getCreateUser() {
-        return createUser;
-    }
-
-    public void setCreateUser(String createUser) {
-        this.createUser = createUser == null ? null : createUser.trim();
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getUpdateUser() {
-        return updateUser;
-    }
-
-    public void setUpdateUser(String updateUser) {
-        this.updateUser = updateUser == null ? null : updateUser.trim();
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Integer getVersion() {
-        return version;
-    }
-
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
-
-    @Override
-    public String toString() {
-        return "LogMessage{" +
-                "id=" + id +
-                ", type=" + type +
-                ", content='" + content + '\'' +
-                ", source='" + source + '\'' +
-                ", status=" + status +
-                ", createUser='" + createUser + '\'' +
-                ", createTime=" + createTime +
-                ", updateUser='" + updateUser + '\'' +
-                ", updateTime=" + updateTime +
-                ", version=" + version +
-                '}';
-    }
 }
--- src/main/java/test/MybatisTest.java
+++ src/main/java/test/MybatisTest.java
@@ -3,9 +3,11 @@ package test;
 import com.lipeng.dao.LogMessageMapper;
 import com.lipeng.domain.LogMessage;
 import com.lipeng.domain.LogMessageExample;
-import java.util.ArrayList;
+import com.lipeng.domain.LogMessageExample.Criteria;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +34,8 @@ public class MybatisTest {
         // 使用全量insert的时候 如果数据库设置了不能为null 那么这里对象每个参数必须有值，不会使用默认值
         // insertSelective只有当你没有给列赋值 mysql才会去使用默认值
         LogMessage message = LogMessage.builder().type(1).content("消息1111").source("uc").status(1)
-                .createUser("lipeng").createTime(date).updateUser("lipeng").updateTime(date).version(1)
+                .createUser("lipeng").createTime(date).updateUser("lipeng").updateTime(date)
+                .version(1)
                 .build();
         result = mapper.insert(message);
         System.out.println(result);
@@ -57,15 +60,41 @@ public class MybatisTest {
     @Test
     public void test() {
         LogMessageExample example = new LogMessageExample();
-        example.createCriteria().andIdIn(Arrays.asList(1L, 2L));
-        mapper.countByExample(example);
+        example.createCriteria().andIdIn(Arrays.asList(1L, 2L, 3L));
+        long count = mapper.countByExample(example);
+        System.out.println(count);
+
+        List<LogMessage> logMessages = mapper.selectByExample(example);
+        logMessages.forEach(System.out::println);
 
         LogMessage logMessage = mapper.selectByPrimaryKey(1L);
         System.out.println(logMessage);
 
+        logMessage.setType(2);
+        mapper.updateByPrimaryKey(logMessage);
+        System.out.println(mapper.selectByPrimaryKey(logMessage.getId()));
+
         logMessage.setType(1);
         logMessage.setUpdateTime(new Date());
         mapper.updateByPrimaryKeySelective(logMessage);
+        System.out.println(mapper.selectByPrimaryKey(logMessage.getId()));
+    }
+
+    @Test
+    public void delete() {
+        int result = mapper.deleteByPrimaryKey(3L);
+        System.out.println(result);
+        LogMessageExample example = new LogMessageExample();
+        example.or().andTypeEqualTo(1).andStatusEqualTo(1);
+        example.or().andStatusEqualTo(0).andSourceEqualTo("sc");
+        result = mapper.deleteByExample(example);
+        System.out.println(result);
+
+        example.clear();
+
+        example.createCriteria().andIdIn(Collections.singletonList(4L));
+        result = mapper.deleteByExample(example);
+        System.out.println(result);
     }
 
 }
